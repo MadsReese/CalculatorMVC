8436bcf861d4accb58d18a715d542bf7986e8e5a
Mads Reese <mre@blablabla.local>
2017-12-16 11:50:37 +0100

    first commit

---
+++ CalculatorMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatorMVC.Models;
+using CalculatorLib;
+
+namespace CalculatorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+		public IActionResult Index()
+		{
+			return View(new CalculateViewModel());
+		}
+
+		[HttpPost]
+		public IActionResult Index(CalculateViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				Calculator calc = new Calculator();
+				model.Result = calc.Add(model.FirstNumber, model.SecondNumber);
+			}
+			return View(model);
+		}
+	}
+}
---
+++ CalculatorMVC/Models/CalculateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CalculatorMVC.Models
+{
+    public class CalculateViewModel
+    {
+		public int FirstNumber { get; set; }
+		public int SecondNumber { get; set; }
+		public decimal Result { get; set; }
+	}
+}
