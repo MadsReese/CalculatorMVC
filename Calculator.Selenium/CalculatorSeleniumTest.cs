e2f1a7d79dff59b63ff7f43fb1806400d4d5dbfb
MadsReese <mre@easv.dk>
2017-12-17 20:26:36 +0100

    Changed to Gecko after realizing PhantomJS "worked"

--- Calculator.Selenium/CalculatorSeleniumTest.cs
+++ Calculator.Selenium/CalculatorSeleniumTest.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Support.UI;
 
@@ -28,7 +29,7 @@ namespace Calculator.Selenium
             // path where the driver can find the chromedriver.exe file. It was
             // added automatically to the bin folder by the Nuget package.
             //driver = new ChromeDriver(Environment.CurrentDirectory);
-            driver = new PhantomJSDriver();
+            driver = new FirefoxDriver();
 			baseURL = "http://localhost:5001/";
 			verificationErrors = new StringBuilder();
 		}
