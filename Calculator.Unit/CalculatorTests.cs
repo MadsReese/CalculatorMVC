558cf1b17232b7dfc23fa75ff2479a6d2c528e6d
MadsReese <mre@easv.dk>
2017-12-17 16:33:01 +0100

    UnitTests One successful One failing

---
+++ Calculator.Unit/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        //Passing test
+        [Test]
+        public void Add1and1equals2()
+        {
+            //Arrange
+            var calc = new CalculatorLib.Calculator();
+            int expected = 2;
+
+            //Act
+            int result = calc.Add(1, 1);
+
+            //Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        //Failing test
+        [Test]
+        public void Add1and1equals3()
+        {
+            //Arrange
+            var calc = new CalculatorLib.Calculator();
+            int expected = 3;
+
+            //Act
+            int result = calc.Add(1, 1);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
